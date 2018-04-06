e55d7eebb18a464eae94445f63167e0ddfd5b33c
FlorianBetoulle <36449077+FlorianBetoulle@users.noreply.github.com>
2018-04-06 11:35:14 +0200

    l

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include "grman/grman.h"
+#include <iostream>
+
+#include "test.h"
+
+int main()
+{
+    /// A appeler en 1er avant d'instancier des objets graphiques etc...
+    grman::init();
+    //MELANIE
+    /// Le nom du répertoire où se trouvent les images à charger
+    grman::set_pictures_path("pics");
+//ok
+    /// Un exemple d'assemblage de widgets
+    Thing t;
+
+    /// Vous gardez la main sur la "boucle de jeu"
+    /// ( contrairement à des frameworks plus avancés )
+    while ( !key[KEY_ESC] )
+    {
+        /// Il faut appeler les méthodes d'update des objets qui comportent des widgets
+        t.update();
+
+        /// Mise à jour générale (clavier/souris/buffer etc...)
+        grman::mettre_a_jour();
+    }
+
+
+
+
+
+
+    //
+
+    //ok
+    grman::fermer_allegro();
+
+    return 0;
+}
+END_OF_MAIN();
+
+
